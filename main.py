cefd2826ba03936f7b5f38403704a61d040e77b3
Kunjan024 <ckunjan886@gmail.com>
2025-09-30 22:14:19 -0400

    Re-initialize repo including full Project 1 folder

---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def validate():
+    try:
+        data = request.get_json(force=True)
+        if "device_id" in data and "timestamp" in data:
+            return jsonify({"status": "valid"}), 200
+        else:
+            return jsonify({"error": "Missing fields"}), 400
+    except Exception:
+        return jsonify({"error": "Invalid JSON"}), 400
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8080)
